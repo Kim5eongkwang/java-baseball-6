3517830705c13ef4e8b437610e82d8deb62e5c5a
Kim5ungkwang <ksg6736@jbnu.ac.kr>
2024-09-26 15:18:20 +0900

    Feat: "게임 진행 로직 구현"
    플레이어의 공을 저장하는 클래스 구현

---
+++ src/main/java/baseball/ball/Balls.java
@@ -0,0 +1,8 @@
+package baseball.ball;
+
+public interface Balls {
+    public void init();
+    public Integer getFirstBall();
+    public Integer getSecondBall();
+    public Integer getThirdBall();
+}
