e5e9f01a7ae60ec4878eaac99cd621428736d7ec
Kim5ungkwang <ksg6736@jbnu.ac.kr>
2024-09-30 11:29:56 +0900

    Fix: "라이벌 공 상태 지속 버그 수정"
    게임을 다시 시작할 때 이전 게임의 상태를 지우지 않던 버그 수정

--- src/main/java/baseball/balls/CPURival.java
+++ src/main/java/baseball/balls/CPURival.java
@@ -11,6 +11,7 @@ public class CPURival implements Balls {
 
     @Override
     public void init() {
+        computer.clear();
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!computer.contains(randomNumber)) {
